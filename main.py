d13fd654dd5cb471fc1b6c8cdfff11ee8b61ea72
Aagam Shah <shahaagam04@gmail.com>
2024-05-27 19:09:37 +0530

    New Dataset uploaded Dataset info: Jan 1, 2020 to Jan 1, 2024

--- main.py
+++ main.py
@@ -28,11 +28,13 @@ that can be achieved through evaporative cooling.
 import pandas as pd
 
 # Reading the csv file contents into a pandas DataFrame called "dataset"
-dataset = pd.read_csv("DataSet_from_Jan2024_to_Apr2024.csv")
+weather_data = pd.read_csv("DataSet_from_Jan2024_to_Apr2024.csv")
 
 # Acquiring the revelant data from the dataset into a sub dataset called "required_dataset"
 # This dataset contains the Station, Date of Reading(includes time), Hourly Dry Bulb Temperature, and Hourly Wet Bulb Temperature
-required_dataset = dataset[["STATION","DATE","HourlyDryBulbTemperature","HourlyWetBulbTemperature"]]
+weather_data = weather_data[["STATION","DATE","HourlyDryBulbTemperature","HourlyWetBulbTemperature"]]
+
+# weather_data["Date"] = 
 
 print(required_dataset)
 
