faae371087336d2629f2b22bc0887a4dfa4abc3f
Aagam Shah <shahaagam04@gmail.com>
2024-06-13 12:08:21 +0530

    added arizona.db; modified database.py to create the arizona database

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 # File created by Aagam Shah
-
+# This file's purpose is to interact with the sqlite database.
 """
-This file's purpose is to interact with the sqlite database.
 Definitions: 
 1. Metadata: a container object that keeps together many different 
           features of a database (or multiple databases) being described
@@ -12,12 +11,16 @@ from sqlalchemy import MetaData
 from sqlalchemy import (Table, Column, String, Integer, Float)
 import pandas as pd
 
-# Reading the csv file contents into a pandas DataFrame
-df = pd.read_csv("Database/csv/DataSet_from_Jan_1_2020_to_Jan_1_2024.csv")
+# Environmental Variables:
+DALLAS_DATASET_CSV = "Database/csv/Texas/DataSet_from_Jan_1_2020_to_Jan_1_2024.csv"
+ARIZONA_STATION_METADATA_CSV = "Database/csv/Arizona/station_metadata.csv"
+ARIZONA_DB_URL = "sqlite:///Database/db/arizona.db"
 
+# Reading the Arizona selected WBAN Stations Metadata into a pandas DataFrame
+df = pd.read_csv(ARIZONA_STATION_METADATA_CSV, encoding='cp1252', delimiter="	")
 
 # Created an engine with sqlite driver and the local sqlite file stored in Database Directory
-engine = create_engine('sqlite:///Database/db/texas.db')
+engine = create_engine(ARIZONA_DB_URL)
 
 # Created an instance of Metadata object
 metadata = MetaData()
@@ -27,7 +30,12 @@ connection = engine.connect()
 
 # Creating a Table called Dallas using pd.to_sql()
 # If exists, then replace
-df.to_sql('Dallas', connection, if_exists="replace", index=False)
+df.to_sql('Station Metadata', connection, if_exists="replace", index=False)
 
-# TODO: Make a database for each state and make tables for the cities of that state
 
+
+# TODO-Arizona: Create a table to Metadata
+# TODO-Arizona: Before uploading all the datasets separately, merge all of them into one csv
+# TODO-Arizona: Slice the datatime column into date and time column
+# TODO-Arizona: Create a new column called Year
+# TODO-Arizona: Index by Station Name, Co-ordinates, Station ID, Year
