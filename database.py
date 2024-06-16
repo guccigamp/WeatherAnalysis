296ee3b7882434b8171b0feeb60493d3cd5cf86a
Aagam Shah <shahaagam04@gmail.com>
2024-06-16 18:20:39 +0530

    removed filemerger.py, main.py, texas datasets; moved to jupyter notebook; created the Mean Hourly Temperature dataset for Arizona

--- database.py
+++ database.py
@@ -11,13 +11,17 @@ from sqlalchemy import MetaData
 from sqlalchemy import (Table, Column, String, Integer, Float)
 import pandas as pd
 
-# Environmental Variables:
+# Environment Variables:
 DALLAS_DATASET_CSV = "Database/csv/Texas/DataSet_from_Jan_1_2020_to_Jan_1_2024.csv"
 ARIZONA_STATION_METADATA_CSV = "Database/csv/Arizona/station_metadata.csv"
 ARIZONA_DB_URL = "sqlite:///Database/db/arizona.db"
+ARIZONA_DB_CSV = "Database/csv/Arizona/Arizona.csv"
 
-# Reading the Arizona selected WBAN Stations Metadata into a pandas DataFrame
-df = pd.read_csv(ARIZONA_STATION_METADATA_CSV, encoding='cp1252', delimiter="	")
+# Reading the metadata csv file
+metadata_df = pd.read_csv(ARIZONA_STATION_METADATA_CSV, dtype={'WBAN ID': 'string', 'Station Name': 'string'})
+
+# Reading the master dataset csv file
+master_df = pd.read_csv(ARIZONA_DB_CSV, dtype={'WBAN ID': 'string', 'Station Name': 'string', 'Hourly Wet Bulb Temperature': 'float64', 'Year': 'int64'})
 
 # Created an engine with sqlite driver and the local sqlite file stored in Database Directory
 engine = create_engine(ARIZONA_DB_URL)
@@ -30,11 +34,14 @@ connection = engine.connect()
 
 # Creating a Table called Station Metadata using pd.to_sql()
 # If exists, then replace
-df.to_sql('Station Metadata', connection, if_exists="replace", index=False)
+metadata_df.to_sql('Station Metadata', connection, if_exists="replace", index=False)
+
+# Creating a Table called Dataset using pd.to_sql()
+# If exists, then replace
+master_df.to_sql('Dataset of Last 3 years', connection, if_exists="replace", index=False)
 
 
-# TODO-Arizona: Create a table to Metadata
-# TODO-Arizona: Before uploading all the datasets separately, merge all of them into one csv
-# TODO-Arizona: Slice the datatime column into date and time column
+
+# TODO-Arizona: Spliting the datatime column into date and time column
 # TODO-Arizona: Create a new column called Year
 # TODO-Arizona: Index by Station Name, Co-ordinates, Station ID, Year
